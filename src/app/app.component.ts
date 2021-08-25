3a1d43efad26d94cf838caccbdea6061b7969d66
Julien-Rtg <julien.rittl@gmail.com>
2021-08-25 16:45:40 +0200

    subject observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
-import { fromEvent, Observable, of } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { AsyncSubject, BehaviorSubject, from, fromEvent, interval, Observable, of, ReplaySubject, Subject } from 'rxjs';
+import { map, multicast, refCount } from 'rxjs/operators';
 
 import { Car } from './car';
 import { CarService } from './car.service';
@@ -22,8 +22,9 @@ import { CarService } from './car.service';
           
     ngOnInit() {
         this.getCars();
-        //this.testObservable();
-        this.operatorObservable();
+        // this.testObservable();
+        // this.operatorObservable();
+        this.subjectObservable();
     }
           
     getCars(): void {
@@ -113,7 +114,6 @@ import { CarService } from './car.service';
     }
 
 
-
     operatorObservable(){
         const operatorObs = 
         of(1,2,3)
@@ -130,8 +130,122 @@ import { CarService } from './car.service';
             complete(){ 'done' },
         });
        
+        
+        setTimeout(() => { // CANCEL THE SUBSCRIBE AFTER 2000MS
+            subscribe.unsubscribe();
+        }, 2000);
+    }
+
+    subjectObservable() {
+
+        // const subject = new Subject<number>();
+
+        // subject.subscribe({
+        //     next(x) { console.log(`Observer A : ${x} `) }
+        // });
+        // subject.subscribe({
+        //     next(x) { console.log(`Observer B : ${x} `) }
+        // });
+
+        // subject.next(1); WILL RETURN A:1 B:1 A:2 B:2
+        // subject.next(2);
+
+        // const observable = from([1, 2, 3]); // WILL RETURN A:1 B:1 A:2 B:2 A:3 B:3
+        // observable.subscribe(subject);
+
+
+        /////// MULTICAST //////////
+
+        // const source = interval(500);
+        // const subject = new Subject();
+        // const refCounted = source.pipe(multicast(subject), refCount());
+        // let subscription1:any, subscription2:any;
+
+
+        // console.log('ObserverA subscribed');
+        // subscription1 = refCounted.subscribe({
+        //     next: (v) => console.log(`Observer A : ${v}`)
+        // });
+
+        // setTimeout(() => {
+        //     console.log('ObserverB subscribed');
+        //     subscription2 = refCounted.subscribe({
+        //         next: (v) => console.log(`Observer B : ${v}`)
+        //     });
+        // }, 600)
+
+        // setTimeout(() => {
+        //     console.log('ObserverA unsubscribed');
+        //     subscription1.unsubscribe();
+        // }, 1200)
+
+        // setTimeout(() => {
+        //     console.log('ObserverB unsubscribed');
+        //     subscription2.unsubscribe();
+        // }, 2000)
+
+        ////// BEHAVIOR SUBJECT /////
+        // It stores the latest value emitted to its consumers, and whenever a new Observer subscribes, it will immediately receive the "current value" from the BehaviorSubject.
+
+        // const subject = new BehaviorSubject(0);
+
+        // subject.subscribe({
+        //     next: (v) => console.log(`ObserverA : ${v}`)
+        // });
+
+        // subject.next(1);
+        // subject.next(2);
+
+        // subject.subscribe({
+        //     next: (v) => console.log(`ObserverB : ${v}`)
+        // });
+
+        // subject.next(3);
+
+
+        //////// REPLAY SUBJECT ///////
+        //  ReplaySubject records multiple values from the Observable execution and replays them to new subscribers.
+
+        // const subject = new ReplaySubject(3);
+
+        // subject.subscribe({
+        //     next: (v) => console.log(`ObserverA ${v}`)
+        // });
+
+        // subject.next(1);
+        // subject.next(2);
+        // subject.next(3);
+        // subject.next(4);
+
+        // subject.subscribe({
+        //     next: (v) => console.log(`ObserverB ${v}`)
+        // })
+
+        // subject.next(5);
+
+        ////////// ASYNC SUBJECT /////////////
+        // The AsyncSubject is a variant where only the last value of the Observable execution is sent to its observers, and only when the execution completes.
+
+        // const subject = new AsyncSubject();
+
+        // subject.subscribe({
+        //     next: (v) => console.log(`ObserverA : ${v}`)
+        // });
+
+        // subject.next(1);
+        // subject.next(2);
+        // subject.next(3);
+        // subject.next(4);
+
+        // subject.subscribe({
+        //     next:(v) => console.log(`ObserverB : ${v}`)
+        // });
+
+        // subject.next(5);
+        // subject.complete();
+
+
     }
 
 
-    
 }
